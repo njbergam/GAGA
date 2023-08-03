89b482c9aa0604152b6347dcd53f7839569b5997
Noah Bergam <34481818+nbergam2021@users.noreply.github.com>
2023-08-03 09:40:31 -0400

    updates

--- Functions.py
+++ Functions.py
@@ -19,9 +19,10 @@ from pykrige.ok import OrdinaryKriging
 import random
 from scipy.interpolate import LinearNDInterpolator
 from matplotlib.animation import FuncAnimation
+import math
 
 ### IMPORTANT BOUNDING BOXES
-r = 2
+r = 1
 jakobs = [69.10-r, 69.10+r, -49.5-r, -49.5+r]
 helheim = [66.21-r,66.21+r, -38.12-r,-38.12+r]
 
@@ -57,7 +58,74 @@ class Graph:
         self.y_spec = self.data_spec['lat']
         self.z_spec = self.data_spec['elev']
         self.t_spec = self.data_spec['time_day']
-        self.bbox = []
+        self.bbox = [min(self.x),max(self.x),min(self.y),max(self.y)]
+
+
+    # output a directed graph on the specialized points
+
+    def digraph(self, tamed=False, gran=100):
+        x = np.array(self.x_spec)
+        y = np.array(self.y_spec)
+        z = np.array(self.z_spec)
+
+        coordinates = np.column_stack([x,y])
+        rad=0.05
+
+        if not tamed:
+            #A1 = radius_neighbors_graph(coordinates, rad, mode='connectivity', include_self=False).toarray()
+            A = kneighbors_graph(coordinates, 1, mode='connectivity', include_self=False).toarray()
+            #A = np.minimum(A1,A2)
+            for i in tqdm(range(len(A))):
+                for j in range(len(A[i])):
+                    if A[i,j]!=0 and z[i]-z[j]>0:
+                        A[i,j] = A[i,j]*(z[i]-z[j])
+                    else:
+                        A[i,j] = 0
+            print(A)
+
+            return nx.from_numpy_matrix(A, create_using = nx.DiGraph()), z, coordinates
+
+
+        else:
+            test_x = np.linspace(self.bbox[2], self.bbox[3], gran)
+            test_y = np.linspace(self.bbox[0], self.bbox[1], gran)
+            pairs = []
+            for x in test_x:
+                for y in test_y:
+                    pairs.append([x,y])
+            pairs = greenland_filter(pairs)
+            xy = np.array(pairs)
+            values, error = self.krige_given(xy)
+
+            assert len(values) == len(xy)
+            A = kneighbors_graph(xy, 4, mode='connectivity', include_self=False).toarray()
+            #embed()
+            mat = np.tile(values, (len(A),1) ) - np.tile(values, (len(A),1) ).T
+            mat = mat<0
+            A = A*mat.astype(int)
+
+            #for i in range(len(A)):
+            #    for j in range(len(A[i])):
+            #        if A[i,j]!=0 and values[i]-values[j]>0:
+            #            A[i,j] = A[i,j]*(values[i]-values[j])
+            #        else: 
+            #            A[i,j] = 0
+                    #if A[i,j] == 0:
+                    #    continue
+                    #else:
+                    #    A[i,j] = values[i]
+
+            return nx.from_numpy_matrix(A, create_using = nx.DiGraph()), values, xy
+        
+
+
+
+
+
+    def info(self):
+        print('number of total points:', len(self.data))
+        print('number of points in ' + str(self.time_window) + ': ' +  str(len(self.data_spec)))
+        print('bbox', self.bbox)
 
     # (lat_min, lon_min, lat_max, lon_max)
     def bbox_specialize(self,bbox, add_label=''):
@@ -97,11 +165,13 @@ class Graph:
         x = self.x_spec
         y = self.y_spec
         z = self.z_spec
+
         if len(x) > 1000:
             indices = random.sample(list(range(len(x))), 1000)
             x = np.take(self.x_spec, indices)
             y = np.take(self.y_spec, indices)
             z = np.take(self.z_spec, indices)
+
         OK = OrdinaryKriging(x,y,z,variogram_model="spherical")
         zpred, error = OK.execute("points", test_coords[:,0], test_coords[:,1])
         return zpred, error
@@ -150,7 +220,7 @@ class Graph:
 
         return pairs, zpred, error
 
-    def krige_tamed(self, saveto, toplot=False):
+    def krige_tamed(self, saveto, toplot=False, ):
         points, zpred, error = self.krige(resolution=50, downsample=False)
         tri = Delaunay(points)
         f1 = figure(figsize=(6,8))
@@ -161,8 +231,9 @@ class Graph:
 
         # higher resolution for the linear interp
         test_x = np.linspace(min(x), max(x), 100)
-        test_y = np.linspace(min(y), max(y), 100)
+        test_y = np.linspace(min(y), max(y), 100) 
         pairs = []
+        shift = (test_x[1]-test_x[0])/2
         for x in test_x:
             for y in test_y:
                 pairs.append([x,y])
@@ -192,9 +263,9 @@ class Graph:
 
     # three types: radius, k-nn, triangle, or normal
     def visualize(self,saveto, tp='radius'):
-        x = self.x_spec
-        y = self.y_spec
-        z = self.z_spec
+        x = np.array(self.x_spec)
+        y = np.array(self.y_spec)
+        z = np.array(self.z_spec)
 
         if tp == 'knn':
             coordinates = np.column_stack([x,y])
@@ -205,6 +276,16 @@ class Graph:
             plt.xlabel('longitude')
             plt.ylabel('latitude')
             plt.plot()
+
+        elif tp == 'trisurf':
+            ax = plt.figure().add_subplot(projection='3d')
+            ixs = np.where(z>200)[0]
+            plt.title('Raw Altitude Map of Jakobs')
+            plt.xlabel('longitude')
+            plt.ylabel('latitude')
+            #plt.zlabel('latitude')
+            ax.plot_trisurf(x[ixs], y[ixs], z[ixs], linewidth=0.2, antialiased=True)
+            plt.show()
 
         elif tp == 'triangle':
             # downsample: otherwise it will take too long
@@ -253,28 +334,38 @@ class Graph:
 
         coord = np.column_stack([x,y])
         coordinates = coord
-        rad = 0.007
+        rad = 0.05
 
         A1 = radius_neighbors_graph(coordinates, rad, mode='connectivity', include_self=False).toarray()
-        A2 = kneighbors_graph(coordinates, 10, mode='connectivity', include_self=False).toarray()
+        A2 = kneighbors_graph(coordinates, 20, mode='connectivity', include_self=False).toarray()
         A = np.minimum(A1,A2)
         #A = radius_neighbors_graph(coordinates, rad, mode='distance', include_self=False).toarray()
-        ixs = np.where(sum(A)>max(sum(A))-2)[0]
-        print(sum(A))
-        print(ixs)
+
+
+        # argmax with conditions
+        ixs = np.argpartition(sum(A), -20)[-20:] #np.where(sum(A)>max(sum(A))-3)[0]
+        #print(sum(A))
+        #print(ixs)
 
         pts = coordinates[ixs]
         plt.close()
         
         G=nx.from_numpy_array(A)
         positions = dict(zip(G.nodes, coordinates))
-        nx.draw(G, pos=positions, node_size=.5, node_color="b")
-        plt.scatter(x,y,c=z,s=0.1)
+
+        figure, axes = plt.subplots()
+        #nx.draw(G, pos=positions, node_size=.5, node_color="b")
+        plt.scatter(x,y,c=z,s=4)
 
         plt.scatter(pts[:,0], pts[:,1],c='r')
+        Drawing_colored_circle = plt.Circle(( -49.5 , 69 ), rad, alpha=0.2 )
+        axes.add_artist( Drawing_colored_circle )
+        #plt.scatter([-49.5], [69], s=)
         plt.savefig(saveto+'.png')
     
         return pts[:,0], pts[:,1]
+
+
 
 
 def time_interp(z_s, num_times):
@@ -295,8 +386,8 @@ def alps_interpolate(title, bbox):
     # going to do a specific location for now
 
     # PICK GRID POINTS ahead of time NOW
-    test_x = np.linspace(gr.bbox[2], gr.bbox[3], 100)
-    test_y = np.linspace(gr.bbox[0], gr.bbox[1], 100)
+    test_x = np.linspace(gr.bbox[2], gr.bbox[3], 500)
+    test_y = np.linspace(gr.bbox[0], gr.bbox[1], 500)
     pairs = []
     for x in test_x:
         for y in test_y:
@@ -412,16 +503,17 @@ def main():
     gr = pd.read_pickle("greenland_clean.pkl") 
     period = [2003,2004]
     gr = Graph(gr, period)
-    gr.bbox_specialize(helheim,add_label='helheim')
+    gr.bbox_specialize(jakobs,add_label='jakobs')
+    gr.visualize(tp='trisurf', saveto='tests/jakobs_trisurf')
 
     
     #gr.visualize(tp='knn', saveto='tests/nn_full_greenland')
     #gr.visualize(tp='triangle', saveto='tests/triangle_greenland')
     #gr.visualize(tp='radius', saveto='tests/radius_full_greenland')
     #gr.visualize(tp='normal', saveto='tests/normal_greenland')
-    for i in range(5):
-        gr.krige_tamed()
-        gr.shift_time_window(1)
+    #for i in range(5):
+    #    gr.krige_tamed()
+    #    gr.shift_time_window(1)
     ## Visualizing...
     # Greenland
     # Jakobshavn Glacier
@@ -435,4 +527,4 @@ def main():
     # Krige-tamed Delaunay
 
 #main()
-alps_interpolate('Whole 2003-2009', bbox=[-73.297, 60.03676, -12.20855, 83.64513])
+#alps_interpolate('Whole 2003-2009', bbox=[66.21-10,66.21+10, -38.12-10,-38.12+10])#[-73.297, 60.03676, -12.20855, 83.64513])
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 The Graph-theoretic Altimetry model of Greenland and Antarctica (GAGA) provides a new, flexible data structure to process the laser altimetry measurements of the IceSAT mission.
 
+Some of the functionality of this model includes:
